7834b5daca431960fbe23456c86c468edb4e979f
seoseungyeon <seosy019@gmail.com>
2017-06-03 21:11:02 +0900

    added 'info'

--- js/main.js
+++ js/main.js
@@ -5,5 +5,18 @@ setpage = function(pageIndex) {
   //active selected link
   $('#link' + pageIndex).addClass('active');
 
-  
+
+  var s = '';
+  switch (pageIndex) {
+    case 97:
+      s += '서승연 (Seo Seung Yeon)<br>';
+      s += '이화여자대학교 조소<br>';
+      s += 'Goldsmiths Unversity MFA fine art';
+
+      break;
+  }
+
+  $('#pageContent').html(s);
+
+
 }
